2ffbec63f7ea2f7b689d1356df16745f6523ca52
hengzhi-zhang <hengzhi.zhang03@gmail.com>
2023-08-14 00:56:15 -0500

    Added no labels on bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,29 @@ import 'screens/home_page.dart';  // Ensure paths are correct
 import 'screens/friends_page.dart';
 import 'screens/settings_page.dart';
 import 'screens/splash_screen.dart';
-import 'person.dart'; // This assumes you have created splash_screen.dart as shown in a previous answer.
+import 'person.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  runApp(const MyApp());
+
+
+ // This assumes you have created splash_screen.dart as shown in a previous answer.
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyD4v7aJO6mnJ-WL-d1QFLNX8CmXRMRLhxM',
+    appId: '1:1074506106271:ios:ade2ea51437bc60aeca9aa',
+    messagingSenderId: '1074506106271',
+    projectId: 'yoke-f9158',
+    storageBucket: 'yoke-f9158.appspot.com',
+    iosClientId: '1074506106271-baoj226s7ntnjjdabta215ebceu2062b.apps.googleusercontent.com',
+    iosBundleId: 'com.example.yokeApp4'
+    ),
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -64,13 +83,19 @@ class _MyHomePageState extends State<MyHomePage> {
             _currentIndex = index;
           });
         },
+        showSelectedLabels: false,  // Hide the labels for selected items
+        showUnselectedLabels: false,  // Hide the labels for unselected items
+        selectedItemColor: Colors.yellow[800],  // Dark yellow for the selected item
+        unselectedItemColor: Colors.grey,  // Default color for unselected items, you can adjust this if needed
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.people), label: 'Matches'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: "Partners"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings"),
         ],
       ),
     );
   }
 }
+
+
 
